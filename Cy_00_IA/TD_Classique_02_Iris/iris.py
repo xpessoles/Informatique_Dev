4286a8a5497bbd55f81fb2b72cb6c27e252114ed
Xavier Pessoles <xpessoles.ptsi@free.fr>
2020-07-26 07:56:03 +0200

    Update iris.py

--- Cy_00_IA/TD_Classique_02_Iris/iris.py
+++ Cy_00_IA/TD_Classique_02_Iris/iris.py
@@ -1,4 +1,6 @@
 from sklearn import datasets
+import matplotlib.pyplot as plt
+
 iris = datasets.load_iris()
 
 import seaborn as sns
@@ -11,3 +13,4 @@ df['label'] = df.apply(lambda x: iris['target_names'][int(x.target)], axis=1)
 df.head()
 
 sns.pairplot(df, hue='label', vars=iris['feature_names'], height=2);
+plt.show()
