8bc33cb6679285732e44825e8cdeb87174929b18
Xavier Pessoles <xpessoles.ptsi@free.fr>
2020-12-17 22:29:33 +0100

    maj

---
+++ Introduction_Arbres/programmes/graphes.py
@@ -0,0 +1,9 @@
+
+
+
+# Première implémentation de graphe par des listes
+"""
+On implémente ici un graphe sous la forme de liste pour laquelle chaque élément 
+est constitué d'un noeud ainsi que de la liste des ces fils
+"""
+graphe = [['A',['B','C']],['B',['D','E']]]
