11ceafb8760838908deaa9867c797b68034a4731
Xavier Pessoles <xpessoles.ptsi@free.fr>
2021-02-05 22:03:58 +0100

    maj

--- Cy_00_IA/TD_Classique_02_Iris/iris.py
+++ Cy_00_IA/TD_Classique_02_Iris/iris.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 
 iris = datasets.load_iris()
 
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-
 sns.set()
-df = pd.DataFrame(data, columns=iris['feature_names'] )
-df['target'] = target
+
+# Conversion des données en DataFrame Panda pour les afficher avec seaborn
+df = pd.DataFrame(iris.data, columns=iris['feature_names'] )
+df['target'] = iris.target
 df['label'] = df.apply(lambda x: iris['target_names'][int(x.target)], axis=1)
 df.head()
 
-sns.pairplot(df, hue='label', vars=iris['feature_names'], height=2);
+sns.pairplot(df, hue='label', vars=iris['feature_names'], height=2)
 plt.show()
